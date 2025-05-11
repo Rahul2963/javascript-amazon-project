ead0f354f3eb208199127964e211b4689b127ff1
--Rahul2963 <--rahulsaran648@gmail.com>
2025-05-11 17:54:18 +0530

    Learn MOre details about classes  and this

--- data/products.js
+++ data/products.js
@@ -51,6 +51,34 @@ class Clothing extends Product{
     return `<a href="${this.sizeChartLink}" target="_blank">Size Chart</a>`
   }
 }
+/*
+const date = new Date();
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+}
+*/
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+object3.method();
+*/
 
 export const products = [
   {
