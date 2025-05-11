f71fe38b1af2cee55cf67dcebcb0d7006a8f13f6
--Rahul2963 <--rahulsaran648@gmail.com>
2025-05-11 19:22:12 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,10 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
-import '../data/cart-class.js';
-import '../data/backend-practice.js';
-renderOrderSummary();
-renderPaymentSummary();
+import { loadProducts } from "../data/products.js";
+
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+//import '../data/cart-class.js';
+//import '../data/backend-practice.js';
