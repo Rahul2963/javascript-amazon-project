c290dcf0400a251ecd9ceaa3a527caa362994a93
--Rahul2963 <--rahulsaran648@gmail.com>
2025-05-13 20:48:40 +0530

    convert from promises to ansync await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,20 @@ import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { loadProducts,loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
+async function loadPage() {
+    await loadProductsFetch();
+
+    await new Promise((resolve) => {
+        loadCart(() => {
+           resolve();
+        });
+    })
+
+    renderOrderSummary();
+    renderPaymentSummary();
+}
+loadPage();
+
 Promise.all([
      loadProductsFetch(),
      new Promise((resolve) => {
