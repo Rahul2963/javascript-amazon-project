a56ffc654405ae9d638c79abe5d95259ac3e0095
--Rahul2963 <--rahulsaran648@gmail.com>
2025-05-13 19:19:42 +0530

    Learn promises and convert cocde into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,53 @@
 import { renderOrderSummary } from "./checkout/ordersummary.js";
 import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+           resolve('value1');
+        });
+     }),
+     new Promise((resolve) => {
+        loadCart(() => {
+           resolve();
+        });
+    })
+
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+
+}).then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary(); 
+})
+*/
+
+/*
+loadProducts(() => {
+   loadCart(() => {
+      renderOrderSummary();
+      renderPaymentSummary();
+    });
+});
+*/
+    
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js';
